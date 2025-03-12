bed733b2344aa81e125f32c878db712f9bb18161
GiapQuang <89502309+GiapQuang@users.noreply.github.com>
2025-03-12 15:55:24 +0700

    Update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import GenerateVideoScreen from '../screens/GenerateVideoScreen';
+import PromptTextScreen from '../screens/PromptTextScreen';
+import PromptImageScreen from '../screens/PromptImageScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +11,12 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="GenerateVideo" component={GenerateVideoScreen} />
+        <Stack.Screen
+          name="GenerateVideoScreen"
+          component={GenerateVideoScreen}
+        />
+        <Stack.Screen name="PromptTextScreen" component={PromptTextScreen} />
+        <Stack.Screen name="PromptImageScreen" component={PromptImageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
